38fe57857f83b0b2c37b62e3ec7ef2d109dbc28e
JOON <joonblue@CARRERA.local>
2016-10-03 10:29:27 -0700

    Intent tested

--- mobile/src/main/java/com/example/joonblue/playground_android/MainActivityFragment.java
+++ mobile/src/main/java/com/example/joonblue/playground_android/MainActivityFragment.java
@@ -1,15 +1,19 @@
 package com.example.joonblue.playground_android;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    public final static String EXTRA_MESSAGE = "com.example.joonblue.playground_android.MESSAGE";
 
     public MainActivityFragment() {
     }
@@ -18,5 +22,13 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(getActivity(), DisplayMessageActivity.class);
+        EditText editText = (EditText) getView().findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
